a00db267505dade5cc0621dd2c97ba362575192f
jemojeremmy2@gmail.com <jemojeremmy2>
2024-05-22 14:29:52 +0300

    Implemented data structures functions

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        matches = []
+        sorted_word = sorted(self.word)
+        for candidate in word_list:
+            if sorted(candidate) == sorted_word:
+                matches.append(candidate)
+        return matches
